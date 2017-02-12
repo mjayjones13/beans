3e05fa6ab1b8ad313459657952c68f152e849827
Josh Cox <cox.josh.d@gmail.com>
2017-02-12 12:52:28 -0600

    Add a seeds file to have a consistent test database

---
+++ dev/db/seed.js
@@ -0,0 +1,34 @@
+var User = require("../../db/user");
+
+function removeAllUsers() {
+    // Remove all documents
+    // Be sure to include a callback function to the remove method
+    User.remove({}, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("lol success");
+        }
+    });
+}
+
+function seedUserCollection() {
+    var josh = new User({
+        username: "josh",
+        password: "josh",
+        friends: []
+    });
+
+    var mike = new User({
+        username: "mike",
+        password: "mike",
+        friends: ["josh"]
+    });
+
+    josh.save();
+    mike.save();
+}
+
+removeAllUsers();
+seedUserCollection();
